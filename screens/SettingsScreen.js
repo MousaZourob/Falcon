88b05c476963e200494780b5e8f4c9f39bf16616
MousaZourob <mousazourob@gmail.com>
2020-07-25 16:08:14 -0400

    added styling to settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
   return <ExpoConfigView />;
 }
 
 SettingsScreen.navigationOptions = {
   title: 'Settings',
+  headerStyle: {
+    backgroundColor: '#1e222d',
+  },
+  headerTitleStyle: {
+      color: 'white'
+  }
 };
